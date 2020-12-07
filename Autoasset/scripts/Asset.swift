fa681d8cc3ac4fc42e18effd449ece5a9816f241
林翰 <linhan@dxy.cn>
2020-12-07 11:16:39 +0800

    chore: 代码优化

--- Autoasset/scripts/Asset.swift
+++ Autoasset/scripts/Asset.swift
@@ -13,13 +13,13 @@ class Asset {
     
     let config: AssetModel
     let nameFormatter = NameFormatter(split: [" ", "_", "-"])
-
+    
     class SplitImageResult {
         var imageFilePaths: [String: [FilePath]] = [:]
         var pdfsFilePaths: [FilePath] = []
         var gifFilePaths: [FilePath] = []
     }
-
+    
     enum Placeholder {
         static let imageBundleName = "[image_bundle_name]"
         static let gifBundleName   = "[gif_bundle_name]"
@@ -40,12 +40,11 @@ class Asset {
     var gifCode:   [String] = []
     var dataCode:  [String] = []
     var colorCode: [String] = []
-    var fontCode:  [String] = []
-
+    
     init(config: AssetModel) {
         self.config = config
     }
-
+    
     func run() throws {
         /// 文件清理
         [config.codes,
@@ -56,7 +55,7 @@ class Asset {
          config.colors]
             .compactMap({ $0 })
             .forEach { try? FilePath(url: $0.output, type: .folder).delete() }
-
+        
         if let resource = config.clear {
             RunPrint.create(titleDesc: "command", title: "clear", level: .info)
             try resource.inputs.forEach {
@@ -66,60 +65,13 @@ class Asset {
             }
             RunPrint.createEnd()
         }
-
-        if let resource = config.xcassets {
-            RunPrint.create(titleDesc: "command", title: "xcassets", level: .info)
-            let output = try FilePath(url: resource.output, type: .folder)
-            try resource.inputs
-                .compactMap({ try FilePath(url: $0, type: .folder) })
-                .forEach {
-                    RunPrint.create(row: "copying")
-                    RunPrint.create(row: "from: \($0.path)")
-                    RunPrint.create(row: "to  : \(output.path)")
-                    try $0.copy(to: output)
-                }
-            RunPrint.createEnd()
-        }
-
-        if let resource = config.codes {
-            RunPrint.create(titleDesc: "command", title: "codes", level: .info)
-            let output = try FilePath(url: resource.output, type: .folder)
-            try resource.inputs
-                .compactMap({ try FilePath(url: $0, type: .folder) })
-                .forEach {
-                    RunPrint.create(row: "copying")
-                    RunPrint.create(row: "from: \($0.path)")
-                    RunPrint.create(row: "to  : \(output.path)")
-                    try $0.copy(to: output)
-                }
-            RunPrint.createEnd()
-        }
-
-        /// 文件创建
-        if let resource = config.images {
-            RunPrint.create(titleDesc: "command", title: "images", level: .info)
-            try Xcassets(config: resource, use: .image).run().forEach { code in
-                self.add(toImage: code)
-            }
-            RunPrint.createEnd()
-        }
-
-        if let resource = config.gifs {
-            RunPrint.create(titleDesc: "command", title: "gifs", level: .info)
-            try Xcassets(config: resource, use: .data).run().forEach { code in
-                self.add(toGIF: code)
-            }
-            RunPrint.createEnd()
-        }
-
-        if let resource = config.colors {
-            RunPrint.create(titleDesc: "command", title: "colors", level: .info)
-            try Xcassets(config: resource, use: .color).run().forEach { code in
-                self.add(toColor: code)
-            }
-            RunPrint.createEnd()
-        }
-
+        
+        try runCopyFiles(title: "xcassets", resource: config.xcassets)
+        try runCopyFiles(title: "codes", resource: config.codes)
+        try runXcasset(title: "images", xcasset: config.images, type: .image, callback: { self.add(toImage: $0) })
+        try runXcasset(title: "colors", xcasset: config.colors, type: .color, callback: { self.add(toColor: $0) })
+        try runXcasset(title: "gifs", xcasset: config.gifs, type: .data, callback: { self.add(toGIF: $0) })
+        
         try output()
     }
     
@@ -128,46 +80,81 @@ class Asset {
             RunPrint("Config: asset/output 不能为空")
             return
         }
-
+        
         var message = template.text
             .replacingOccurrences(of: Placeholder.images, with: imageCode.sorted().joined(separator: "\n"))
             .replacingOccurrences(of: Placeholder.gifs, with: gifCode.sorted().joined(separator: "\n"))
             .replacingOccurrences(of: Placeholder.datas, with: dataCode.sorted().joined(separator: "\n"))
             .replacingOccurrences(of: Placeholder.colors, with: colorCode.sorted().joined(separator: "\n"))
-            .replacingOccurrences(of: Placeholder.fonts, with: fontCode.sorted().joined(separator: "\n"))
-
+        
         if let config = config.images, let bundleName = config.bundleName {
             message = message.replacingOccurrences(of: Placeholder.imageBundleName, with: bundleName)
         }
-
+        
         if let config = config.gifs, let bundleName = config.bundleName {
             message = message.replacingOccurrences(of: Placeholder.gifBundleName, with: bundleName)
         }
-
+        
         if let config = config.datas, let bundleName = config.bundleName {
             message = message.replacingOccurrences(of: Placeholder.dataBundleName, with: bundleName)
         }
-
+        
         if let config = config.colors, let bundleName = config.bundleName {
             message = message.replacingOccurrences(of: Placeholder.colorBundleName, with: bundleName)
         }
-
+        
         let data = message.data(using: .utf8)
         let file = try FilePath(url: template.output, type: .file)
         try file.delete()
         try file.create(with: data)
     }
+    
+}
 
+private extension Asset {
+
+    func runCopyFiles(title: String, resource: AssetModel.Resource?) throws {
+        guard let resource = resource else {
+            return
+        }
+        RunPrint.create(titleDesc: "command", title: title, level: .info)
+        let output = try FilePath(url: resource.output, type: .folder)
+        try resource.inputs
+            .compactMap({ try FilePath(url: $0, type: .folder) })
+            .forEach {
+                RunPrint.create(row: "copying")
+                RunPrint.create(row: "from: \($0.path)")
+                RunPrint.create(row: "to  : \(output.path)")
+                try $0.copy(to: output)
+            }
+        RunPrint.createEnd()
+    }
+    
+    func runXcasset(title: String,
+                    xcasset: AssetModel.Xcasset?,
+                    type: Xcassets.ResourceType,
+                    callback: (AssetCode) -> Void) throws {
+        guard let xcasset = xcasset else {
+            return
+        }
+        
+        RunPrint.create(titleDesc: "command", title: title, level: .info)
+        try Xcassets(config: xcasset, use: type).run().forEach { code in
+            callback(code)
+        }
+        RunPrint.createEnd()
+    }
+    
 }
 
 // MARK: - add
-extension Asset {
-
+private extension Asset {
+    
     func add(toColor code: AssetCode) {
         guard let text = config.template?.colorCode else {
             return
         }
-
+        
         let variableName = nameFormatter.variableName(code.variableName, prefix: config.colors?.variablePrefix)
         let str = text.replacingOccurrences(of: Placeholder.variableName, with: variableName.uppercased())
             .replacingOccurrences(of: Placeholder.mark, with: code.color.mark)
@@ -175,15 +162,15 @@ extension Asset {
             .replacingOccurrences(of: Placeholder.name2, with: code.color.dark)
         colorCode.append(str)
     }
-
+    
     func add(toImage code: AssetCode) {
         guard let text = config.template?.imageCode else {
             return
         }
         let variableName = nameFormatter.variableName(code.variableName, prefix: nil)
         imageCode.append(text
-            .replacingOccurrences(of: Placeholder.variableName, with: variableName)
-            .replacingOccurrences(of: Placeholder.name1, with: code.xcassetName))
+                            .replacingOccurrences(of: Placeholder.variableName, with: variableName)
+                            .replacingOccurrences(of: Placeholder.name1, with: code.xcassetName))
     }
     
     func add(toGIF code: AssetCode) {
@@ -192,8 +179,8 @@ extension Asset {
         }
         let variableName = nameFormatter.variableName(code.variableName, prefix: nil)
         gifCode.append(text
-            .replacingOccurrences(of: Placeholder.variableName, with: variableName)
-            .replacingOccurrences(of: Placeholder.name1, with: code.xcassetName))
+                        .replacingOccurrences(of: Placeholder.variableName, with: variableName)
+                        .replacingOccurrences(of: Placeholder.name1, with: code.xcassetName))
     }
     
 }
