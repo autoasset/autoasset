c0ec087c8d0867f4733d4362311c910a9917a091
linhey <is.linhey@outlook.com>
2021-10-14 14:29:12 +0800

    限定依赖库版本

--- Package.swift
+++ Package.swift
@@ -25,11 +25,11 @@ let package = Package(
     name: Autoasset,
     platforms: [ .macOS(.v10_15) ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-log.git", from: "1.4.2"),
-        .package(url: "https://github.com/linhay/Stem.git", .branch("master")),
-        .package(url: "https://github.com/jpsim/Yams.git", from: "4.0.6"),
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.1"),
-        .package(url: "https://github.com/kareman/SwiftShell.git", from: "5.1.0")
+        .package(url: "https://github.com/apple/swift-log.git", .upToNextMajor(from: "1.4.2")),
+        .package(url: "https://github.com/linhay/Stem.git", .upToNextMajor(from: "1.0.0")),
+        .package(url: "https://github.com/jpsim/Yams.git", .upToNextMajor(from: "4.0.6")),
+        .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMajor(from: "1.0.1")),
+        .package(url: "https://github.com/kareman/SwiftShell.git", .upToNextMajor(from: "5.1.0"))
     ],
     targets: [
         .target(name: AutoassetModels, dependencies: [
